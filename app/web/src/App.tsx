0fb6727591b8a00890b151db1621f20a46a522c2
alvist <alvist@amdocs.com>
2024-07-07 15:23:43 -0400

    refactor(web): format

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
         <div class="content">
             <h1>Rsbuild with Solid</h1>
             <p>Start building amazing things with Rsbuild.</p>
-                            <p>server running in port: {config.port} </p>
+            <p>server running in port: {config.port} </p>
         </div>
     )
 }
