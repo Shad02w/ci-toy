e35859021651b0036d9da0f11ad494aca8b5948b
Shad02w <alvistse123@gmail.com>
2024-10-29 17:21:22 -0400

    feat(web): use cyan

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css"
 
 const App = () => {
     return (
-        <div class="content yellow">
+        <div class="content cyan">
             <h1>Rsbuild with Solid</h1>
             <p>Start building amazing things with Rsbuild.</p>
             <p>server running in port: {config.port} </p>
